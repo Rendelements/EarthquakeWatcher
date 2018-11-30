b39482b6b6f719ee7ad353a24392d4e14e50dc7e
Simon Withington <s.withington@icloud.com>
2018-11-30 22:24:19 +1100

    Created view models

---
+++ EarthquakeWatcher/ViewModels/ListViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  ListViewModel.swift
+//  EarthquakeWatcher
+//
+//  Created by Simon Withington on 30/11/18.
+//  Copyright © 2018 RendElements. All rights reserved.
+//
+
+import Foundation
+
+class ListViewModel {
+    
+    init() {
+        
+        
+    }    
+}
+
---
+++ EarthquakeWatcher/ViewModels/MapViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  MapViewModel.swift
+//  EarthquakeWatcher
+//
+//  Created by Simon Withington on 30/11/18.
+//  Copyright © 2018 RendElements. All rights reserved.
+//
+
+import Foundation
+
+class MapViewModel {
+    
+    init() {
+        
+        
+    }
+}
