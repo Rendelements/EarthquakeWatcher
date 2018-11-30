f5aa07a2b1295b60ed7d6302edfb403db0dcb35e
Simon Withington <s.withington@icloud.com>
2018-11-30 21:15:26 +1100

    Initial Commit

---
+++ EarthquakeWatcher/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  EarthquakeWatcher
+//
+//  Created by Simon Withington on 30/11/18.
+//  Copyright © 2018 RendElements. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ EarthquakeWatcher/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  EarthquakeWatcher
+//
+//  Created by Simon Withington on 30/11/18.
+//  Copyright © 2018 RendElements. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
